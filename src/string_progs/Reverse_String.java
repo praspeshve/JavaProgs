d4fc2974096a9cdaf9c5aceae3805a0b912a50ec
praspeshve <praspeshve@gmail.com>
2019-04-24 12:42:42 +0530

    JavaPrograms

---
+++ src/string_progs/Reverse_String.java
@@ -0,0 +1,32 @@
+package string_progs;
+
+import java.util.Scanner;
+
+public class Reverse_String {
+
+	public static String rev_string(String str){
+		
+		String rev = "";
+		int len = str.length();
+		for(int i=len-1; i>=0; i--){
+			char ch = str.charAt(i);
+			rev = rev+ch;
+			
+		}
+		
+		
+		
+		return rev;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any String: ");
+		String str = sc.next();
+		System.out.println("The reverse of the given string is: "+ rev_string(str));
+	}
+
+}
